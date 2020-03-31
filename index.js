0f8710314861c149ecebf7379498c348d983fff2
edan <emanueledancelli@gmail.com>
2020-03-31 23:23:40 +0200

    chore: updated package.json

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const newCmd = require("./lib/new");
 prog
   .version("0.0.1")
   .command("new", "Create a new sketch")
-  .argument("<title>", "The title for your sketch")
+  .argument("<title>", "The sketch title")
   .action(newCmd);
 
 prog.parse(process.argv);
